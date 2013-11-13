71c2ca16e329eab72f66ac46f1f15f0d84dc3b39
alvations <alvations@gmail.com>
2013-11-13 16:37:24 +0000

    added the language families from ethnologue in pickle form

--- ethnologue.py
+++ ethnologue.py
@@ -14,24 +14,26 @@ ethnologue_domain = "http://www.ethnologue.com/"
 language_families = defaultdict(list)
 
 for line in fin.readlines():
+  line = line.decode('utf-8')
   if  '''<div class="views-field views-field-name">''' in line:
     langfamlink = bs(line).find('a').get('href')
     langfamily = bs(line).get_text().strip()
-    #print langfamily, langfamlink
+    print langfamily, langfamlink
     for line2 in urllib2.urlopen(langfamlink):
+      line2 = line2.decode('utf-8')
       if '''<span class="views-field views-field-field-iso-639-3">''' in line2:
-        langlink = ethnologue_domain+bs(line2).find('a').get('href')
         lang = bs(line2).get_text().strip()
-        langname = re.sub(r'[\[\(][^)]*[\]\)]', '', lang)
+        if lang.strip() == "": print line2
+        langname = re.sub(r'[\[\(\<][^)]*[\]\)\>]', '', lang)
         isocode = re.findall(r'\[([^]]*)\]',lang)[0]
         geo = re.findall(r'\([^)]*\)',lang)[0].rpartition(" ")[2][:-1]
+        print isocode, langname , geo
         language_families[langfamily].append((isocode, langname , geo))
-    break
 
-with codecs.open('languagefamilies','wb') as fout:
+with codecs.open('languagefamilies.pk','wb') as fout:
   pickle.dump(language_families, fout)
 
-with codecs.open('languagefamilies','rb') as fin2: 
+with codecs.open('languagefamilies.pk','rb') as fin2: 
   language_families = pickle.load(fin2)
   for lf in language_families:
     for l in language_families[lf]:
