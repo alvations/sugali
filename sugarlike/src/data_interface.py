6f78c549d6a9a282f0d0c4e855f8c0cf266f685c
alvations <alvations@gmail.com>
2013-11-29 12:38:51 +0100

    debugged import and additional slash

--- sugarlike/src/data_interface.py
+++ sugarlike/src/data_interface.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 
-DATADIR = '../data/'
+import codecs
 
+DATADIR = '../data'
 DATASOURCE = ['wikpedia','omniglot','odin','udhr','crubadan']
 
 # TODO: this returns the language code used for the individual data source.
 def magic_iso_func(datasource,iso):
-  pass
+  return iso # placeholder
 
 def get_data(datasource=None, iso=None, datatype=None):
   languagecode = magic_iso_func(datasource, iso)
   filename =  '/'.join([DATADIR,datasource,languagecode,datatype])
+  print filename
   return codecs.open(filename,'r','utf8')
 
-
+''' 
+#Informal test
 get_data(DATASOURCE[2],'deu','1gram')
+'''
