6c08795127be51a808cb897077ac81fff1755250
alvations <alvations@gmail.com>
2014-01-17 17:52:06 +0100

    added evaluate.py

---
+++ sugarlike/evaluate.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+def tenfold(data_source, randseed=0):
+  import random, sys
+  from extractfeature import get_features, sentence2ngrams
+  from universalcorpus import odin, omniglot, udhr
+  random.seed(randseed)
+  corpus = list(locals()[data_source].source_sents())
+  corpus = sorted(iter(corpus), key=lambda k: random.random())
+  totrain = int(len(corpus) * 90 / 100)
+  yield corpus[:totrain] # train data
+  yield corpus[totrain:] # test data
+  
+import sys  
+for i in tenfold('odin'):
+  for j in i:
+    print j
+  
