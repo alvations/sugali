9298faeff47f5e8af9deed5cdfe7a0247377d508
Susanne Fertmann <s9sufert@stud.uni-saarland.de>
2014-01-22 14:11:10 +0100

    Add test for word2ngrams.

--- sugarlike/test/test_extractfeature.py
+++ sugarlike/test/test_extractfeature.py
@@ -10,7 +10,12 @@ from extractfeature import *
 
 class TestExtractfeature(unittest.TestCase):
   sentence = 'Dies ist ein Beispielsatz zum Extrahieren.'
-  ngrams = ['Die', 'ies', 'ist', 'ein', 'Bei', 'eis', 'isp', 'spi', 'pie',
+  
+  hallo_trigrams = ['Hal', 'all', 'llo']
+  hallo_bigrams = ['Ha', 'al', 'll', 'lo']
+  
+  zerograms = []
+  trigrams = ['Die', 'ies', 'ist', 'ein', 'Bei', 'eis', 'isp', 'spi', 'pie',
             'iel', 'els', 'lsa', 'sat', 'atz', 'zum', 'Ext', 'xtr', 'tra',
             'rah', 'ahi', 'hie', 'ier', 'ere', 'ren', 'en.']
   onegrams = ['D', 'i', 'e', 's', 'i', 's', 't', 'e', 'i', 'n', 'B', 'e', 'i',
@@ -25,30 +30,51 @@ class TestExtractfeature(unittest.TestCase):
   fivegrams = ['Beisp', 'eispi', 'ispie', 'spiel', 'piels', 'ielsa', 'elsat',
                'lsatz', 'Extra', 'xtrah', 'trahi', 'rahie', 'ahier', 'hiere',
                'ieren', 'eren.']
-  zerograms = []
-  ngrams_bound = ['<Di', 'Die', 'ies', 'es>', '<is', 'ist', 'st>', '<ei', 'ein',
-               'in>', '<Be', 'Bei', 'eis', 'isp', 'spi', 'pie', 'iel', 'els',
-               'lsa', 'sat', 'atz', 'tz>', '<zu', 'zum', 'um>', '<Ex', 'Ext',
-               'xtr', 'tra', 'rah', 'ahi', 'hie', 'ier', 'ere', 'ren', 'en.',
-               'n.>']
+  trigrams_bound = ['<Di', 'Die', 'ies', 'es>', '<is', 'ist', 'st>', '<ei',
+                    'ein', 'in>', '<Be', 'Bei', 'eis', 'isp', 'spi', 'pie',
+                    'iel', 'els', 'lsa', 'sat', 'atz', 'tz>', '<zu', 'zum',
+                    'um>', '<Ex', 'Ext', 'xtr', 'tra', 'rah', 'ahi', 'hie',
+                    'ier', 'ere', 'ren', 'en.', 'n.>']
   words = ['Dies', 'ist', 'ein', 'Beispielsatz', 'zum', 'Extrahieren.']
   words_bound = ['<Dies>', '<ist>', '<ein>', '<Beispielsatz>', '<zum>',
                '<Extrahieren.>']
+             
+               
+  def test_word2ngrams(self):
+    self.assertEquals(self.hallo_trigrams, word2ngrams('Hallo',option='char'))
+    self.assertEquals(self.hallo_trigrams, word2ngrams('Hallo'))
+    self.assertEquals(self.hallo_bigrams,  word2ngrams('Hallo', n=2))
+    self.assertEquals(self.zerograms,      word2ngrams('Hallo', n=-1))
+    self.assertEquals(self.zerograms,      word2ngrams('Hallo', n=7))
+    self.assertEquals(self.zerograms,      word2ngrams('Hallo', n=0))
+    self.assertEquals(self.hallo_trigrams, word2ngrams('Hallo',
+                                             option='3grams'))
+
 
   def test_sentence2ngrams(self):
-    self.assertEquals(self.ngrams, sentence2ngrams(self.sentence))
-    self.assertEquals(self.onegrams, sentence2ngrams(self.sentence, n=1))
-    self.assertEquals(self.bigrams, sentence2ngrams(self.sentence, n=2))
-    self.assertEquals(self.ngrams, sentence2ngrams(self.sentence, n=3))
+    self.assertEquals(self.trigrams,  sentence2ngrams(self.sentence))
+    self.assertEquals(self.onegrams,  sentence2ngrams(self.sentence, n=1))
+    self.assertEquals(self.bigrams,   sentence2ngrams(self.sentence, n=2))
+    self.assertEquals(self.trigrams,  sentence2ngrams(self.sentence, n=3))
     self.assertEquals(self.zerograms, sentence2ngrams(self.sentence, n=0))
     self.assertEquals(self.zerograms, sentence2ngrams(self.sentence, n=-1))
-    self.assertEquals(self.ngrams_bound, sentence2ngrams(self.sentence, with_word_boundary=True))
-    self.assertEquals(self.ngrams_bound, sentence2ngrams(self.sentence, n=3, with_word_boundary=True))
-    self.assertEquals(self.zerograms, sentence2ngrams(self.sentence, n=0, with_word_boundary=True))
-    self.assertEquals(self.zerograms, sentence2ngrams(self.sentence, n=-1, with_word_boundary=True))
-    self.assertEquals(self.words_bound, sentence2ngrams(self.sentence, option='word', with_word_boundary=True))
-    self.assertEquals(self.words, sentence2ngrams(self.sentence, option='word', with_word_boundary=False))
-    self.assertEquals(self.onegrams + self.bigrams + self.ngrams + self.fourgrams + self.fivegrams, sentence2ngrams(self.sentence, option='allgrams'))
+    self.assertEquals(self.zerograms, sentence2ngrams(self.sentence, n=0, 
+                                                     with_word_boundary=True))
+
+    self.assertEquals(self.zerograms, sentence2ngrams(self.sentence, n=-1,
+                                                     with_word_boundary=True))
+    self.assertEquals(self.trigrams_bound, sentence2ngrams(self.sentence, 
+                                                     with_word_boundary=True))
+    self.assertEquals(self.trigrams_bound, sentence2ngrams(self.sentence, n=3,
+                                                     with_word_boundary=True))
+    self.assertEquals(self.words_bound, sentence2ngrams(self.sentence, 
+                                      option='word', with_word_boundary=True))
+    self.assertEquals(self.words, sentence2ngrams(self.sentence, option='word',
+                                                     with_word_boundary=False))
+
+    self.assertEquals(self.onegrams + self.bigrams + self.trigrams + 
+                      self.fourgrams + self.fivegrams, 
+                      sentence2ngrams(self.sentence, option='allgrams'))
  
    
 if __name__ == "__main__":
