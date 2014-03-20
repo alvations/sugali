9715c1ea6de482d32f31acc6d146c2b2ada042ca
alvations <alvations@gmail.com>
2014-03-20 10:48:32 +0100

    cleanup the code a little.

--- universalcorpus/langcluster.py
+++ universalcorpus/langcluster.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 
+import cPickle as pickle
+import time, os, ast, random,codecs
+
+from math import sqrt, acos
 from collections import Counter, defaultdict
 from itertools import chain, product
 from operator import itemgetter
 from threading import Thread
 from Queue import Queue
-import time, os, ast, random,codecs
 
 import numpy as np
 import pandas as pd
-import h5py
-from sklearn import cluster
-import milk
-from math import sqrt, acos
-import cPickle as pickle
+##from sklearn import cluster; import milk
+import scipy
+import matplotlib.pylab as plt
 
 from crawlandclean import odin, omniglot, udhr, ethnologue
 from miniethnologue import ISO2LANG, MACRO2LANG, RETIRED2ISO
+
 
 random.seed(0)
 
@@ -36,8 +38,6 @@ def normalize(featurevector, length=1):
   """
   from math import sqrt
   try:
-    ##print length, type(length)
-    ##print featurevector.values()
     norm = length / sqrt(sum([x*x for x in featurevector.values()]))
   except ZeroDivisionError:
     return featurevector
@@ -45,15 +45,29 @@ def normalize(featurevector, length=1):
     featurevector[feat] *= norm
   return featurevector
 
+dead = {"osp":"Old Spanish", "odt":"Old Dutch", "goh": "Old High German",
+        "got":"Gothic","wlm":"Middle Welsh","oge":"Old Georgian",
+        "tpn":"Tupinamba","ojp":"Old Japanese","sga":"Old Irish",
+        "hit":"Hittite","tkm":"Takelma","dum":"Middle Dutch",
+        "fro":"Old French","nci":"Classical Nahuatl","gmh":"Middle High German",
+        "mxi":"Mozarabic"}
+macro_split = {"nob":"Norwegian, Bokmaal","nno":"Norwegian Nynorsk"} # nor.
+constructed = {"ido":"Ido","tlh":"Klingon","tzl":"Talossan","jbo":"Lojban",
+               "ina":"Interlingua"}
+
+living_languages = set(ISO2LANG.keys()) - set(RETIRED2ISO.keys()) - \
+set(dead.keys()) - set(constructed.keys()) - set(MACRO2LANG.keys()) - \
+set(macro_split.keys())
+
 def generate_ngrams(data_source):
   twograms = defaultdict(Counter) 
   threegrams = defaultdict(Counter)
   unigrams = defaultdict(Counter)
   for lang, sent in globals()[data_source].source_sents():
-    twograms[lang].update(Counter(sent2ngrams(sent,2))) ## TODO: normalize all ngrams in the 
+    if lang not in living_languages: continue;
+    twograms[lang].update(Counter(sent2ngrams(sent,2))) 
     threegrams[lang].update(Counter(sent2ngrams(sent,3)))
     unigrams[lang].update(Counter(["<word>"+i+"</word>" for i in sent.split()]))
-
   return [twograms, threegrams, unigrams]
 
 def wrapper(func, arg, queue):
@@ -61,108 +75,48 @@ def wrapper(func, arg, queue):
   queue.put(func(arg))
 
 def load_ngram_array():
-  if os.path.exists('language.tag') and \
-  os.path.exists('tocluster.data.npy') and \
-  os.path.exists('all_features'):
-    data = np.load('tocluster.data.npy')
-    languages = ast.literal_eval(open('language.tag','r').read())
-    all_features = codecs.open('all_features','r','utf8').read().split()
-    return data, languages, all_features
-  else:
-    print "Accessing data..."
-    data_sources = ['odin','omniglot','udhr']
-    # Parallel processing hocus-pocus.     
-    q1, q2, q3 = Queue(), Queue(), Queue()
-    Thread(target=wrapper, args=(generate_ngrams, data_sources[0], q1)).start() 
-    Thread(target=wrapper, args=(generate_ngrams, data_sources[1], q2)).start() 
-    Thread(target=wrapper, args=(generate_ngrams, data_sources[2], q3)).start()
-    odin_ngrams = q1.get(); omniglot_ngrams = q2.get(); udhr_ngrams = q3.get()
+  print "Accessing data..."
+  data_sources = ['odin','omniglot','udhr']
+  # Parallel processing hocus-pocus.     
+  q1, q2, q3 = Queue(), Queue(), Queue()
+  Thread(target=wrapper, args=(generate_ngrams, data_sources[0], q1)).start() 
+  Thread(target=wrapper, args=(generate_ngrams, data_sources[1], q2)).start() 
+  Thread(target=wrapper, args=(generate_ngrams, data_sources[2], q3)).start()
+  odin_ngrams = q1.get(); omniglot_ngrams = q2.get(); udhr_ngrams = q3.get()
+  
+  # Combining the ngrams.
+  print "combining ngrams..."
+  unigrams = defaultdict(Counter)
+  for k,v in chain(odin_ngrams[2].iteritems(), omniglot_ngrams[2].iteritems(), 
+                   udhr_ngrams[2].iteritems()):
+    unigrams[k].update(v)
+  
+  twograms = defaultdict(Counter)
+  for k,v in chain(odin_ngrams[0].iteritems(), omniglot_ngrams[0].iteritems(), 
+                   udhr_ngrams[0].iteritems()):
+    twograms[k].update(v)
+  
+  threegrams = defaultdict(Counter)
+  for k,v in chain(odin_ngrams[1].iteritems(), omniglot_ngrams[1].iteritems(), 
+                   udhr_ngrams[1].iteritems()):
+    threegrams[k].update(v)
+  
+  for lang in twograms:
+    twograms[lang] = normalize(twograms[lang], 1/sqrt(3))
+    threegrams[lang] = normalize(threegrams[lang], 1/sqrt(3))
+    unigrams[lang] = normalize(unigrams[lang], 1/sqrt(3))
     
-    # Combining the ngrams.
-    print "combining ngrams..."
-    unigrams = defaultdict(Counter)
-    for k,v in chain(odin_ngrams[2].iteritems(), omniglot_ngrams[2].iteritems(), 
-                     udhr_ngrams[2].iteritems()):
-      unigrams[k].update(v)
-    
-    twograms = defaultdict(Counter)
-    for k,v in chain(odin_ngrams[0].iteritems(), omniglot_ngrams[0].iteritems(), 
-                     udhr_ngrams[0].iteritems()):
-      twograms[k].update(v)
-    
-    threegrams = defaultdict(Counter)
-    for k,v in chain(odin_ngrams[1].iteritems(), omniglot_ngrams[1].iteritems(), 
-                     udhr_ngrams[1].iteritems()):
-      threegrams[k].update(v)
-    
-    for lang in twograms:
-      twograms[lang] = normalize(twograms[lang], 1/sqrt(3))
-      threegrams[lang] = normalize(threegrams[lang], 1/sqrt(3))
-      unigrams[lang] = normalize(unigrams[lang], 1/sqrt(3))
-    allgrams = defaultdict(Counter)
-    for k,v in chain(twograms.iteritems(), threegrams.iteritems(), unigrams.iteritems()):
-      allgrams[k].update(v)
-    
-    return allgrams
-    
-    '''
-    fout = codecs.open('tocluto.data','w','utf8')
-    for lang, ng in allngrams.iteritems():
-      print>>fout, lang+" "+" ".join(list(chain(*[[feature]*count for feature, count in ng.iteritems()])))
-    '''
-    '''
-    # Casting inputs into numpy arrays.
-    print "casting to numpy arrays"
-    start = time.time()
-    all_features = sorted(set(chain(*[allngrams[i].keys() for i in allngrams])))
-    languages = []; data = []
-    for lang, ng in sorted(allngrams.iteritems()):
-      ##print lang, ng
-      languages.append(lang)
-      x = [ng[i] for i in all_features]
-      data.append(x)
-    ##print len(data), len(all_features)
-    print "converting to numpy arrays"
-    ##data = pd.lib.to_object_array(data).astype(float) # converts counters to array.
-    ##data = np.array(data)
-    ##np.save('tocluster.data',data)
-    ##filearray = h5py.File('sugali.data','w')
-    ##data = filearray.create_dataset('tocluster',(len(data),len(data[0])),dtype='f')
-    ##data[...] = data
-    
-    print>>open('language.tag','w'), languages
-    print>>codecs.open('all_features','w'), " ".join(all_features)
-    print time.time() - start
-    '''
-    return data, languages, all_features
+  allgrams = defaultdict(Counter)
+  for k,v in chain(twograms.iteritems(), threegrams.iteritems(), 
+                   unigrams.iteritems()):
+    allgrams[k].update(v)  
+  return allgrams
 
 def distance(c1, c2):
   dimsum = sum(c1[i]*c2[i] for i in set(c1.keys()).intersection(c2.keys()))
   if dimsum >= 1.0: 
-    print dimsum
     return 0
   return acos(dimsum)
-
-if not os.path.exists('ngram.data'):
-  data = load_ngram_array()
-  with open('ngram.data','wb') as ngramout:
-    pickle.dump(data, ngramout)
-else:
-  with open('ngram.data','rb') as fin:
-    data = pickle.load(fin)
-
-dmatrix = []
-if not os.path.exists('distance.out'):    
-  fout = open('distance.out','w')
-  for l1,l2 in product(data.keys(), data.keys()):
-    dist = distance(data[l1],data[l2])
-    dmatrix.append((l1,l2,dist))
-    print>>fout, "\t".join([l1, l2, str(dist)])
-else:
-  for line in open('distance.out','r'):
-    l1, l2, dist = line.strip().split('\t')
-    dist = float(dist)
-    dmatrix.append((l1,l2,dist))
 
 def plot_tree(P, pos=None):
   import matplotlib.pylab as plt
@@ -182,21 +136,35 @@ def plot_tree(P, pos=None):
   plt.ylim(ymin, ymax+01.*abs(ymaxs))
   plt.show()
 
+def avg(l): return sum(l)/float(len(l));
 
-dead = {"osp":"Old Spanish", "odt":"Old Dutch", "goh": "Old High German",
-        "got":"Gothic","wlm":"Middle Welsh","oge":"Old Georgian",
-        "tpn":"Tupinamba","ojp":"Old Japanese","sga":"Old Irish",
-        "hit":"Hittite","tkm":"Takelma","dum":"Middle Dutch",
-        "fro":"Old French","nci":"Classical Nahuatl","gmh":"Middle High German",
-        "mxi":"Mozarabic"}
-macro_split = {"nob":"Norwegian, Bokmaal","nno":"Norwegian Nynorsk"} # nor.
-constructed = {"ido":"Ido","tlh":"Klingon","tzl":"Talossan","jbo":"Lojban",
-               "ina":"Interlingua"}
+###########################################################################
 
-living_languages = set(ISO2LANG.keys()) - set(RETIRED2ISO.keys()) - set(dead.keys()) - set(constructed.keys()) - set(MACRO2LANG.keys()) - set(macro_split.keys())
+# Access subcorpora, extracts ngrams features, normalize counts of ngrams. 
+if not os.path.exists('ngram.data'):
+  data = load_ngram_array()
+  with open('ngram.data','wb') as ngramout:
+    pickle.dump(data, ngramout)
+else:
+  with open('ngram.data','rb') as fin:
+    data = pickle.load(fin)
 
-matrix = {}
-labels = []
+# Calculates distance matrix.
+dmatrix = []
+if not os.path.exists('distance.out'):    
+  fout = open('distance.out','w')
+  for l1,l2 in product(data.keys(), data.keys()):
+    dist = distance(data[l1],data[l2])
+    dmatrix.append((l1,l2,dist))
+    print>>fout, "\t".join([l1, l2, str(dist)])
+else:
+  for line in open('distance.out','r'):
+    l1, l2, dist = line.strip().split('\t')
+    dist = float(dist)
+    dmatrix.append((l1,l2,dist))
+
+# Converts distance matrix into numpy array
+matrix = {}; labels = []
 for l1, l2, dist in dmatrix:
   if l1 in living_languages and l2 in living_languages:
     if l2 not in labels: labels.append(l2)
@@ -204,9 +172,6 @@ for l1, l2, dist in dmatrix:
 
 matrix = [v.values() for k,v in matrix.iteritems()]
 matrix = pd.lib.to_object_array(matrix).astype(float) 
-
-import scipy
-import matplotlib.pylab as plt
 
 methods = 'single complete average weighted centroid ward'.split()
 criterion = 'inconsistent distance maxclust'.split()
@@ -226,26 +191,21 @@ for me, cr in product(methods,criterion):
     lang2clusters[lang_labels] = cluster_num
     cluster2langs[cluster_num].append(lang_labels)
     
-  fouti = open('induce-clusters','w')
+  ##fouti = open('induce-clusters','w')
   precisions, recalls, fscores = [], [], []
   for i in labels:
     gold_class = ethnologue.FAMILIES2ISO[ethnologue.ISO2FAMILY[i]]
     gold_class = [g for g in gold_class if g in living_languages]
     induced_cluster = set(cluster2langs[lang2clusters[i]])
-    print>>fouti, i
-    print>>fouti, induced_cluster
-    print>>fouti, gold_class
-    print>>fouti, "\n"
+    ##print>>fouti, i
+    ##print>>fouti, induced_cluster
+    ##print>>fouti, gold_class
+    ##print>>fouti, "\n"
     overlap = len(induced_cluster.intersection(gold_class))
-    print overlap, len(gold_class)
     rec = overlap /float(len(gold_class))
     prec = overlap /float(len(induced_cluster))
-    precisions.append(prec)
-    recalls.append(rec)
-    
+    precisions.append(prec); recalls.append(rec)
     fscores.append((2*prec*rec)/float(prec+rec))
-    
-  def avg(l): return sum(l)/float(len(l));
   
   print>>fouteval, "\t".join([me, cr, "{0:.5f}".format(avg(precisions)), \
                               "{0:.5f}".format(avg(recalls)), \
